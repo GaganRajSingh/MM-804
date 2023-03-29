02b5aa17728db5108552cc0f8265f066ad4f11cb
Gagan Raj Singh <gaganrajsinghbedi@gmail.com>
2023-03-28 20:21:57 -0600

    Added threshold line in aqi graph

--- src/components/Aqi.js
+++ src/components/Aqi.js
@@ -5,13 +5,15 @@ import values from '../data/aqi_values.json'
 
 function Aqi () {
 
-    const filter = Array.from({ length: 101 }, (_, i) => i / 10);
+    const filter = Array.from({ length: 101 }, (_, i) => i / 10)
+    const [threshold, setThreshold] = useState(0)
     const [visibleData, setVisible] = useState(values.map((value) => {
         return 1
     }))
 
 
     const handleSliderChange = (e) => {
+        setThreshold(parseFloat(e.step.label))
         setVisible(
             values.map((value) => {
                 return value >= parseFloat(e.step.label) ? 1 : 0.3
@@ -21,12 +23,22 @@ function Aqi () {
 
     var data = [
         {
+            name: "AQI",
             x: dates,
             y: values,
             type: 'scatter',
             mode: 'markers',
             marker: {
                 opacity: visibleData
+            }
+        },
+        {
+            name: "Threshold",
+            x: [dates[0], dates[dates.length-1]],
+            y: [threshold, threshold],
+            type: 'line',
+            line: {
+                dash: 'dash'
             }
         }
     ]
@@ -48,7 +60,6 @@ function Aqi () {
                 })
             }
         ],
-        visible: visibleData
     }
 
     return (
