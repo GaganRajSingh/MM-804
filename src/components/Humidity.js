10e813b9c9d4612196d02fd690be2b92f30ef6d9
yanshan24 <yanshan@ualberta.ca>
2023-03-30 13:01:31 -0600

    add year selection

--- src/components/Humidity.js
+++ src/components/Humidity.js
@@ -6,6 +6,7 @@ import humidityData from '../data/ACISDailyData-Humidity.csv';
 
 function Humidity() {
   const [data, setData] = useState([]);
+  const [selectedYear, setSelectedYear] = useState(null);
 
   useEffect(() => {
     Papa.parse(humidityData, {
@@ -17,15 +18,18 @@ function Humidity() {
     });
   }, []);
 
-  const humidityMin = data.map((row) => parseFloat(row.Humidity_Min));
-  const humidityMax = data.map((row) => parseFloat(row.Humidity_Max));
-  const relativeHumidityAvg = data.map((row) => parseFloat(row.Relative_Humidity_Avg));
+  const humidityMin = data.filter((row) => !selectedYear || new Date(row.Date).getFullYear() === selectedYear)
+    .map((row) => parseFloat(row.Humidity_Min));
+  const humidityMax = data.filter((row) => !selectedYear || new Date(row.Date).getFullYear() === selectedYear)
+    .map((row) => parseFloat(row.Humidity_Max));
+  const relativeHumidityAvg = data.filter((row) => !selectedYear || new Date(row.Date).getFullYear() === selectedYear)
+    .map((row) => parseFloat(row.Relative_Humidity_Avg));
 
   const trace1 = {
     y: humidityMin,
     name: 'Humidity Min',
     type: 'violin',
-    side: 'both',
+    side: 'positive',
     box: { visible: true },
     line: { color: '#1f77b4' },
   };
@@ -34,7 +38,7 @@ function Humidity() {
     y: humidityMax,
     name: 'Humidity Max',
     type: 'violin',
-    side: 'both',
+    side: 'positive',
     box: { visible: true },
     line: { color: '#d62728' },
   };
@@ -43,25 +47,38 @@ function Humidity() {
     y: relativeHumidityAvg,
     name: 'Relative Humidity Avg',
     type: 'violin',
-    side: 'both',
+    side: 'positive',
     box: { visible: true },
     line: { color: '#2ca02c' },
   };
 
+  const years = Array.from(new Set(data.map((row) => new Date(row.Date).getFullYear())))
+  .filter((year) => !isNaN(year) && year !== undefined);
+
   return (
-    <Plot
-      data={[trace1, trace2, trace3]}
-      layout={{
-        width: 800,
-        height: 500,
-        title: 'Humidity Distribution',
-        yaxis: {
-          title: 'Humidity (%)',
-        },
-      }}
-    />
+    <div>
+      <Plot
+        data={[trace1, trace2, trace3]}
+        layout={{
+          width: 800,
+          height: 500,
+          title: selectedYear ? `Humidity Distribution for Year ${selectedYear}` : 'Humidity Distribution',
+          yaxis: {
+            title: 'Humidity (%)',
+          },
+        }}
+      />
+      <div style={{ marginTop: '20px' }}>
+        <label htmlFor="year-dropdown">Select Year: </label>
+        <select id="year-dropdown" value={selectedYear} onChange={(e) => setSelectedYear(parseInt(e.target.value))}>
+          <option value={null}>All Years</option>
+          {years.map((year) => (
+            <option key={year} value={year}>{year}</option>
+          ))}
+        </select>
+      </div>
+    </div>
   );
 }
 
 export default Humidity;
-
