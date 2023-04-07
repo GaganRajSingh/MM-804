f29e3d614ab21200e715c0d689e646309ddcd76b
yanshan24 <yanshan@ualberta.ca>
2023-04-07 16:01:08 -0600

    Add sidebar dropdown menus for each graph

--- src/components/App.js
+++ src/components/App.js
@@ -15,27 +15,43 @@ function App() {
       <div className="App">
         <nav className="sidebar">
           <ul>
-            <li><NavLink to="/" exact activeClassName="active">Home</NavLink></li>
-            <li><NavLink to="/temperature" activeClassName="active">Temperature</NavLink></li>
-            <li><NavLink to="/precipitation" activeClassName="active">Precipitation</NavLink></li>
+            <li>
+              <NavLink to="/" exact activeClassName="active">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/temperature" activeClassName="active">Temperature</NavLink>
+              <ul>
+                <li><NavLink to="/temperature/slider" activeClassName="active">Temperature slider</NavLink></li>
+                <li><NavLink to="/temperature/over-years" activeClassName="active">Temperature over years</NavLink></li>
+                <li><NavLink to="/temperature/single-year" activeClassName="active">Temperature of single year</NavLink></li>
+              </ul>
+            </li>
+            <li>
+              <NavLink to="/precipitation" activeClassName="active">Precipitation</NavLink>
+              <ul>
+                <li><NavLink to="/precipitation/distribution" activeClassName="active">Precipitation Distribution</NavLink></li>
+                <li><NavLink to="/precipitation/snow" activeClassName="active">Snow</NavLink></li>
+              </ul>
+            </li>
             <li><NavLink to="/humidity" activeClassName="active">Humidity</NavLink></li>
             <li><NavLink to="/aqi" activeClassName="active">AQI</NavLink></li>
             <li><NavLink to="/wind" activeClassName="active">Wind</NavLink></li>
           </ul>
         </nav>
+
         <main>
           <Routes>
             <Route path="/" element={<div className="home">
               <h1 className="title">Weather Data Visualization</h1>
               <p className="author">By Yanshan Wan and Gagan Singh</p>
             </div>} />
-            <Route path="/temperature" element={<><Surface /><Slicing /><Temp_slider /></>} />
-            <Route path="/precipitation" element={<>              
-              <h2 className="title">Precipitation</h2>
-              <Precipitation />
-              <h2 className="title">Snow</h2>
-              <Snow />
-            </>} />
+            <Route path="/temperature" element={<><Slicing /></>} />
+            <Route path="/temperature/slider" element={<Slicing />} />
+            <Route path="/temperature/over-years" element={<Surface />} />
+            <Route path="/temperature/single-year" element={<Temp_slider />} />
+            <Route path="/precipitation" element={<><Precipitation /></>} />
+            <Route path="/precipitation/distribution" element={<><Precipitation /></>} />
+            <Route path="/precipitation/snow" element={<><Snow /></>} />
             <Route path="/humidity" element={<Humidity />} />
             <Route path="/aqi" element={<Aqi />} />
             <Route path="/wind" element={<Wind />} />
